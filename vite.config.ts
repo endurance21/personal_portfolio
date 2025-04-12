22bf39e31cb0fa60b181eb16df3276f31c2af839
Divyanshu <divyanshu.raj@microsoft.com>
2025-04-12 16:18:21 +0530

    update

--- vite.config.ts
+++ vite.config.ts
@@ -2,10 +2,10 @@ import { defineConfig } from "vite";
 import react from "@vitejs/plugin-react-swc";
 import path from "path";
 import { componentTagger } from "lovable-tagger";
-const repoName = "personal_portofolio";
+
 // https://vitejs.dev/config/
 export default defineConfig(({ mode }) => ({
-  base: mode === "production" ? "/personal_portfolio/" : "/", 
+  base: mode === "production" ? "/" : "/", 
   server: {
     host: "::",
     port: 8080,
